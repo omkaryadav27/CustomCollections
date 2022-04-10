a3414f2f55d90e23cdc69c7bf657de76b5b65257
Admin <omkar.v.yadav27@gmail.com>
2022-04-10 13:20:13 -0400

    Performance enhancements and added concatenation and multiplication functionality

--- cset/sorted_set.py
+++ cset/sorted_set.py
@@ -1,12 +1,15 @@
 from collections.abc import Sequence
 from bisect import bisect_left
+from itertools import chain
+
 
 class SortedSet(Sequence):
-    def __init__(self, items = None):
+    def __init__(self, items=None):
         self._items = sorted(set(items)) if items is not None else []
 
     def __contains__(self, item):
-        return item in self._items
+        index = bisect_left(self._items, item)
+        return (index != len(self._items)) and (self._items[index] == item)
 
     def __len__(self):
         return len(self._items)
@@ -31,16 +34,29 @@ class SortedSet(Sequence):
 
         return self._items != other._items
 
-    def count(self, value) -> int:
+    def __add__(self, other):
+        return SortedSet(chain(self, other))
+
+    def __mul__(self, other):
+        return self if other >= 1 else SortedSet()
+
+    def __rmul__(self, other):
+        return self * other
+
+    def index(self, value) -> int:
         index = bisect_left(self._items, value)
-        found = (index != len(self._items)) and (self._items[index] == value)
-        return int(found)
+        if (index != len(self._items)) and (self._items[index] == value):
+            return index
+        raise ValueError(f'{value} not found')
+
+    def count(self, value) -> int:
+        return int(value in self)
 
 
 if __name__ == '__main__':
     pass
-    #from random import randrange
-    #s = SortedSet(randrange(1000) for _ in range(2000))
-    #from timeit import timeit
-    #timeit(setup='from __main__ import s',\
+    # from random import randrange
+    # s = SortedSet(randrange(1000) for _ in range(2000))
+    # from timeit import timeit
+    # timeit(setup='from __main__ import s',\
     #       stmt = '[s.count(i) for i in range(1000)]', number = 100)
--- cset/test_sorted_set.py
+++ cset/test_sorted_set.py
@@ -1,12 +1,14 @@
 import unittest
+from collections.abc import Sequence, Container, Sized, Iterable
 from sorted_set import SortedSet
+
 
 class MyTestCase(unittest.TestCase):
     def test_empty(self):
         s = SortedSet([])
 
     def test_from_sequence(self):
-        s = SortedSet([7,8,3,1])
+        s = SortedSet([7, 8, 3, 1])
 
     def test_with_duplicates(self):
         s = SortedSet([7, 7, 7])
@@ -24,10 +26,11 @@ class MyTestCase(unittest.TestCase):
     def test_default(self):
         s = SortedSet()
 
+
 class TestContainerProtocol(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.s = SortedSet([6,7,4,5])
+        self.s = SortedSet([6, 7, 4, 5])
 
     def test_positive_contained(self):
         self.assertTrue(6 in self.s)
@@ -41,6 +44,10 @@ class TestContainerProtocol(unittest.TestCase):
     def test_negative_not_contained(self):
         self.assertFalse(6 not in self.s)
 
+    def test_protocol(self):
+        self.assertTrue(issubclass(SortedSet, Container))
+
+
 class TestSizedProtocol(unittest.TestCase):
 
     def test_empty(self):
@@ -49,19 +56,23 @@ class TestSizedProtocol(unittest.TestCase):
 
     def test_one(self):
         s = SortedSet([2])
-        self.assertEqual(len(s),1)
+        self.assertEqual(len(s), 1)
 
     def test_n(self):
         s = SortedSet(range(10))
         self.assertEqual(len(s), 10)
 
     def test_with_duplicates(self):
-        s = SortedSet([5,5,5])
-        self.assertEqual(len(s),1)
+        s = SortedSet([5, 5, 5])
+        self.assertEqual(len(s), 1)
+
+    def test_protocol(self):
+        self.assertTrue(issubclass(SortedSet, Sized))
+
 
 class TestIterableProtocol(unittest.TestCase):
     def setUp(self) -> None:
-        self.s = SortedSet([5,7,2,2,6])
+        self.s = SortedSet([5, 7, 2, 2, 6])
 
     def test_iter(self):
         i = iter(self.s)
@@ -73,14 +84,18 @@ class TestIterableProtocol(unittest.TestCase):
 
     def test_for_loop(self):
         index = 0
-        expected = [2,5,6,7]
+        expected = [2, 5, 6, 7]
         for item in self.s:
             self.assertEqual(item, expected[index])
             index += 1
 
+    def test_protocol(self):
+        self.assertTrue(issubclass(SortedSet, Iterable))
+
+
 class TestSequenceProtocol(unittest.TestCase):
     def setUp(self) -> None:
-        self.s = SortedSet([1,4, 9,13,15])
+        self.s = SortedSet([1, 4, 9, 13, 15])
 
     def test_index_zero(self):
         self.assertEqual(self.s[0], 1)
@@ -93,17 +108,17 @@ class TestSequenceProtocol(unittest.TestCase):
             self.s[5]
 
     def test_index_minus_one(self):
-        self.assertEqual(self.s[-1],15)
+        self.assertEqual(self.s[-1], 15)
 
     def test_index_minus_five(self):
-        self.assertEqual(self.s[-5],1)
+        self.assertEqual(self.s[-5], 1)
 
     def test_index_one_before_beginning(self):
         with self.assertRaises(IndexError):
             self.s[-6]
 
     def test_slice_from_start(self):
-        self.assertEqual(self.s[:3], SortedSet([1,4,9]))
+        self.assertEqual(self.s[:3], SortedSet([1, 4, 9]))
 
     def test_slice_from_end(self):
         self.assertEqual(self.s[3:], SortedSet([13, 15]))
@@ -112,13 +127,13 @@ class TestSequenceProtocol(unittest.TestCase):
         self.assertEqual(self.s[10:], SortedSet())
 
     def test_slice_arbitrary(self):
-        self.assertEqual(self.s[2:4], SortedSet([9,13]))
+        self.assertEqual(self.s[2:4], SortedSet([9, 13]))
 
     def test_full_slice(self):
         self.assertEqual(self.s[:], self.s)
 
     def test_reversed(self):
-        s = SortedSet([4,5,6])
+        s = SortedSet([4, 5, 6])
         r = reversed(s)
         self.assertEqual(next(r), 6)
         self.assertEqual(next(r), 5)
@@ -127,58 +142,95 @@ class TestSequenceProtocol(unittest.TestCase):
             next(r)
 
     def test_index_positive(self):
-        s = SortedSet([4,5,6])
-        self.assertEqual(s.index(5),1)
+        s = SortedSet([4, 5, 6])
+        self.assertEqual(s.index(5), 1)
 
     def test_index_negative(self):
-        s = SortedSet([4,5,6])
+        s = SortedSet([4, 5, 6])
         with self.assertRaises(ValueError):
             s.index(8)
 
     def test_count_zero(self):
-        s = SortedSet([1,5])
+        s = SortedSet([1, 5])
         self.assertEqual(s.count(2), 0)
 
     def test_count_one(self):
-        s = SortedSet([1,5,6,7])
-        self.assertEqual(s.count(5),1)
+        s = SortedSet([1, 5, 6, 7])
+        self.assertEqual(s.count(5), 1)
+
+    def test_protocol(self):
+        self.assertTrue(issubclass(SortedSet, Sequence))
+
+    def test_concatenate_disjoint(self):
+        s = SortedSet([1, 2, 3])
+        t = SortedSet([5, 6, 7])
+        self.assertEqual(s + t, SortedSet([1, 2, 3, 5, 6, 7]))
+
+    def test_concatenate_equal(self):
+        s = SortedSet([1, 2, 3])
+        self.assertEqual(s + s, s)
+
+    def test_concatenate_intersecting(self):
+        s = SortedSet([1, 2, 3])
+        t = SortedSet([3, 4, 5])
+        self.assertEqual(s + t, SortedSet([1, 2, 3, 4, 5]))
+
+    def test_zero_repetitions(self):
+        s = SortedSet([1, 2, 3])
+        self.assertEqual(s * 0, SortedSet())
+
+    def test_multiple_repetitions(self):
+        s = SortedSet([1, 2, 3])
+        self.assertEqual(s * 100, s)
+
+    def test_zero_repetitions_right(self):
+        s = SortedSet([1, 2, 3])
+        self.assertEqual(0 * s, SortedSet())
+
+    def test_multiple_repetitions(self):
+        s = SortedSet([1, 2, 3])
+        self.assertEqual(100 * s, s)
+
 
 class TestReprProtocol(unittest.TestCase):
     def test_repr_empty(self):
         s = SortedSet()
-        self.assertEqual(repr(s),'SortedSet()')
+        self.assertEqual(repr(s), 'SortedSet()')
 
     def test_repr_some(self):
-        s = SortedSet([9,4,6])
-        self.assertEqual(repr(s),'SortedSet([4, 6, 9])')
+        s = SortedSet([9, 4, 6])
+        self.assertEqual(repr(s), 'SortedSet([4, 6, 9])')
+
 
 class TestEqualityProtocol(unittest.TestCase):
     def test_positive_equal(self):
-        self.assertTrue(SortedSet([4,6,5]) == SortedSet([6,4,5]))
+        self.assertTrue(SortedSet([4, 6, 5]) == SortedSet([6, 4, 5]))
 
     def test_negative_equal(self):
-        self.assertFalse(SortedSet([4,6,5]) == SortedSet([8,10,9]))
+        self.assertFalse(SortedSet([4, 6, 5]) == SortedSet([8, 10, 9]))
 
     def test_type_mismatch(self):
-        self.assertFalse(SortedSet([4,5,6]) == [4,5,6])
+        self.assertFalse(SortedSet([4, 5, 6]) == [4, 5, 6])
 
     def test_identity(self):
         s = SortedSet([5, 4])
         self.assertTrue(s == s)
 
+
 class TestInEqualityProtocol(unittest.TestCase):
     def test_positive_unequal(self):
-        self.assertTrue(SortedSet([4,6,5]) != SortedSet([1,2,3]))
+        self.assertTrue(SortedSet([4, 6, 5]) != SortedSet([1, 2, 3]))
 
     def test_negative_equal(self):
-        self.assertFalse(SortedSet([4,6,5]) != SortedSet([4,5,6]))
+        self.assertFalse(SortedSet([4, 6, 5]) != SortedSet([4, 5, 6]))
 
     def test_type_mismatch(self):
-        self.assertTrue(SortedSet([4,5,6]) != [4,5,6])
+        self.assertTrue(SortedSet([4, 5, 6]) != [4, 5, 6])
 
     def test_identity(self):
         s = SortedSet([5, 4])
         self.assertFalse(s != s)
+
 
 if __name__ == '__main__':
     unittest.main()
